27fadfaa41db8fad6331328bea9cf182d80d007c
Franiboy <63066013+Franiboy@users.noreply.github.com>
2020-06-05 09:55:29 +0200

    eingidung Kalender und bisschen Oberfläche

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,5 +1,4 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -7,6 +6,7 @@ import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+
 
 @NgModule({
   imports: [
