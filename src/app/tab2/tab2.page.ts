ff5c588be5bf0e23868f18d1274d03e585b799ab
Denis Grabiger <qo64lak>
2020-06-10 21:36:58 +0200

    formeldarstellung

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EquationString } from './equationString';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+    title: string = 'Addieren';
+    subtitle: string = 'Einstieg ohne Zehnerübergang mit Stellentafel: bis 100 bis 1.000';
+    content: string = 'Hier lassen sich 2 bis 3 Zahlen im dreistelligen sechsstelligen Bereich schriftlich addieren. Für den Übertrag sind die gestrichelten Kästchen vorgesehen.';
+    hideContent = false;
+
+    eqinput: string = " d";
+
+    eqs: EquationString ={
+        text: [ "Das hier ist eine Formel", "0 = (x^2 + y^2 -1)^3 - x^2 y^3", "toll oder?"],
+        mode: [0, 1, 0],
+    };
 
   constructor() {}
+
+  send(){
+    if(!this.hideContent)
+        this.hideContent = true;
+  }
+
+  switchHide(){
+    this.hideContent ? this.hideContent=false : this.hideContent=true;
+
+    console.log(this.eqs.text[0]);
+  }
 
 }
