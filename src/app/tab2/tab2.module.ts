34c2d889af711a7651d80ae293d1243ddb91487c
Johannes <haeuserjohannes@outlook.com>
2020-06-11 17:00:11 +0200

    remove node modules and .idea

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { KatexModule } from 'ng-katex';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
@@ -14,7 +15,8 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    KatexModule
   ],
   declarations: [Tab2Page]
 })
